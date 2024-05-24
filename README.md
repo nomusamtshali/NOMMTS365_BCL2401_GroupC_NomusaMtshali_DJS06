ff9288a3a6e52920e9dcb8847a4406a205ec9280
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-05-24 16:35:05 +0200

    prettier format updated on all files

--- README.md
+++ README.md
@@ -1,39 +1,62 @@
 # DJS06 Project Brief: Higher Order Functions
 
 # Project Brief
+
 - This project was designed to challenge and deepen my understanding of JavaScript's powerful array methods. I had to manipulate data using `forEach`, `map`, `filter`, `reduce`, and more, to perform a variety of tasks. This exercise tested my ability to apply these methods in combination to solve complex problems and achieve desired outcomes.
 
 - The essence of using these higher-order functions was to encourage a declarative approach to programming: I defined what I want to achieve (e.g., filter a list, transform data) without having to specify how to perform the operations step-by-step (as I would with imperative loops like for or while). This led to more readable, concise, and more maintainable code. Additionally, these functions helped avoid side effects and create pure functions, both key aspects of functional programming.
 
 # Project Overview
+
 ### Data Sets
+
 I had to work with two primary datasets:
 
 1. A list of provinces:
+
 ```javascript
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
-```
-2. A list of names:
-```javascript
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
-```
-3. A list of products with prices:
-```javascript
-const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
 ];
 ```
+
+2. A list of names:
+
+```javascript
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
+```
+
+3. A list of products with prices:
+
+```javascript
+const products = [
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+```
+
 #### Exercises
 
 1. **ForEach Basics**: Use `forEach` to log each name and each province to the console. Then, log each name with a matching province in the format "Name (Province)".
 2. **Uppercase Transformation**: Use `map` to create a new array of province names in all uppercase. Log the new array to the console.
 3. **Name Lengths**: Create a new array using `map` that contains the length of each name.
-4. **Sorting**: Use `sort` to alphabetically sort the provinces. 
+4. **Sorting**: Use `sort` to alphabetically sort the provinces.
 5. **Filtering Cape**: Use `filter` to remove provinces containing "Cape". Log the count of remaining provinces.
 6. **Finding 'S'**: Create a boolean array using `map` and `some` to determine if a name contains the letter 'S'.
 7. **Creating Object Mapping**: Use `reduce` to transform the names array into an object mapping names to their respective provinces.
@@ -50,22 +73,26 @@ For these exercises, wrap your logic in a single `console.log` statement:
 6. **Object Transformation**: Using `Object.entries` and `reduce`, recreate the products object with keys 'name' and 'cost', maintaining their original values.
 
 # Elements Included
-- A scripts.js file including my solutions 
+
+- A scripts.js file including my solutions
 - A GitHub repository
 
-# Reflections 
-## Area of Mastery 
+# Reflections
+
+## Area of Mastery
+
 - Grasping the fundamental JavaScript array methods: I effectively used forEach, map, sort, filter, and reduce to manipulate the provided arrays under the first set of excercises.
 
 ## Challenges Faced
+
 - Handling potentially invalid/unavailable data: The products array contained missing prices and I was puzzeled by this. It took some time to work around the 'products' array.
 - Iterating over an array that has missing values
 
 ## Areas of improvement
+
 - Conciseness in some areas: In the advanced exercise, the logic for finding highest and lowest priced items can actually be solved in a simpler way but I hope I'll be able to get it next time.
 - Using the filter, map and reduce methods in one code block.
 
-
 # Overall Learning Experience
-- The project was clear and straight-forward. I've done array methods before but this project/challenge demonstrated a strong foundation in core JavaScript array methods and object manipulation. The advanced excercise was quite difficult as I had to use a single 'console.log' execution. As I mentioned the areas for improvement above and considering potential challenges, I can further enhance my JavaScript skills and ability to handle more complex data and scenarios. 
 
+- The project was clear and straight-forward. I've done array methods before but this project/challenge demonstrated a strong foundation in core JavaScript array methods and object manipulation. The advanced excercise was quite difficult as I had to use a single 'console.log' execution. As I mentioned the areas for improvement above and considering potential challenges, I can further enhance my JavaScript skills and ability to handle more complex data and scenarios.
--- scripts.js
+++ scripts.js
@@ -1,26 +1,40 @@
 // EXERCISES
 
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
 
 // using the forEach method to log each name to the console
 names.forEach((names) => {
-  console.log(names)
+  console.log(names);
 });
 
 // using the forEach method to log each province to the console
 provinces.forEach((provinces) => {
-  console.log(provinces)
+  console.log(provinces);
 });
 
 // using the forEach method to log each name with a matching province in the format "Name (Province)".
 names.forEach((name, index) => {
   const province = provinces[index];
   console.log(`${name} (${province})`);
-}); 
+});
 
 // using the map method to create a new array of province names in all uppercase.
 const uppercaseProvinces = provinces.map((province) => province.toUpperCase());
@@ -28,76 +42,77 @@ console.log(uppercaseProvinces);
 
 // using the map method to create a new array that contains the length of each name
 const nameLengths = names.map((name) => name.length);
-console.log(nameLengths)
+console.log(nameLengths);
 
 // using the sort method to alphabetically sort the provinces
 const sortedProvinces = provinces.sort();
-console.log(sortedProvinces)
+console.log(sortedProvinces);
 
-// using the filter method to remove provinces containing "Cape" and logging the count of remaining provinces 
-const filteredProvinces = provinces.filter((province) => !province.includes("Cape"));
+// using the filter method to remove provinces containing "Cape" and logging the count of remaining provinces
+const filteredProvinces = provinces.filter(
+  (province) => !province.includes("Cape")
+);
 console.log(filteredProvinces);
 
 // using map and some method to determine if a name contains the letter 'S'
-const namesWithS = names.map((name) => name.includes('S'));
-console.log(namesWithS)
+const namesWithS = names.map((name) => name.includes("S"));
+console.log(namesWithS);
 
 // using the reduce method to transform the names array into an object mapping names to their respective provinces
 const nameToProvince = names.reduce((acc, name, index) => {
   acc[name] = provinces[index];
   return acc;
-  }, {});
-  console.log(nameToProvince);
-
+}, {});
+console.log(nameToProvince);
 
 // ADVANCED EXCERCISE
 
 // A list of products with prices:
 const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
 
-console.log (
+console.log(
+  // iterating over the products array
+  products.map((product) => product.product),
 
-// iterating over the products array
-products.map(product => product.product), 
+  // filtering out products with names longer than 5 characters
+  products.filter((product) => product.product.length <= 5),
 
-// filtering out products with names longer than 5 characters
-products.filter(product => product.product.length <= 5),
-
-/* filtering out products without prices
+  /* filtering out products without prices
 converting string prices to numbers 
 calculating the total price of all products using reduce */
-products
-.filter(product => product.price)
-.map(product => ({...product, price: Number(product.price)}))
-.reduce((acc, product) => acc + product.price, 0),
+  products
+    .filter((product) => product.price)
+    .map((product) => ({ ...product, price: Number(product.price) }))
+    .reduce((acc, product) => acc + product.price, 0),
 
-// concatenating all product names into a single string using reduce 
-products.reduce((acc, product) => acc + product.product, ''),
+  // concatenating all product names into a single string using reduce
+  products.reduce((acc, product) => acc + product.product, ""),
 
-// identifying the highest and lowest-prices items
-products.reduce((results, product) => {
-  const price = parseInt(product.price);       
-  if (price) {
-      results.Highest = Math.max(results.Highest || 0, price);   
-    }       
-  if (price) {
-    results.Lowest = Math.min(results.Lowest || Infinity, price);       
-  }       
-  return results;     
-},     
-  {Highest: undefined, Lowest: undefined}),
+  // identifying the highest and lowest-prices items
+  products.reduce(
+    (results, product) => {
+      const price = parseInt(product.price);
+      if (price) {
+        results.Highest = Math.max(results.Highest || 0, price);
+      }
+      if (price) {
+        results.Lowest = Math.min(results.Lowest || Infinity, price);
+      }
+      return results;
+    },
+    { Highest: undefined, Lowest: undefined }
+  ),
 
-// recreating the products object with keys 'name' and 'cost' while maintaining their orignal values 
-Object.entries(products).reduce((acc, [index, { product, price }]) => {     
-  acc[index] = { name: product, cost: price };     
-  return acc;   
-}, []) 
-
+  // recreating the products object with keys 'name' and 'cost' while maintaining their orignal values
+  Object.entries(products).reduce((acc, [index, { product, price }]) => {
+    acc[index] = { name: product, cost: price };
+    return acc;
+  }, [])
 );
